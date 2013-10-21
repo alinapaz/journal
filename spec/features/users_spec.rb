5fdf651cea57f8ededfdc90e5ad6e0dc2867fd36
Angelina Calderon <alinapaz@gmail.com>
2013-10-20 22:41:02 -0700

    comments for a feature spec for user login

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,30 +1,20 @@
-require 'spec_helper'
+# user signs in, is confirmed, uses app as signed in user
+require'spec_helper'
 
-feature 'User management' do 
-  scenario 'adds a new user' do
-    admin = create( :admin)
-
-    visit root_path
-    click_link 'Log In'
-    fill_in 'Email', with: admin.email
-    fill_in 'Password', with: admin.password
-    click_button 'Log In'
-
-    visit root_path
-    expect{
-      click_link 'Users'
-      click_link 'New User'
-      fill_in 'Email', with: 'newuser@example.com'
-      find('#password').fill_in 'Password', with: 'secret123'
-      find('#password_confirmation').fill_in 'Password confirmation', 
-      with: 'secret123'
-      click_button 'Create User'
-      }.to change(User, :count).by(1)
-      expect(current_path).to eq users_path
-      expect(page).to have_content 'New user created'
-      within 'h1' do
-        expect(page).to have_content 'Users'
-      end
-      expect(page).to have_content 'newuser@example.com'
+feature 'user sign in' do 
+  background do
+    # create user
   end
+
+  scenario 'my first test' do 
+    # user comes to app front page
+    # user fills in email field with email
+    # user fills in password field with password
+    # user clicks log in button
+    # user sees listing entries page
+    # user clicks new entry link
+    # user creates new entry
+    # user clicks save button
+    # user sees listing entries page with new entry included
+  end 
 end
