408a9832961e1483ac5dfeb5062affc0cd1167f5
Angelina Calderon <alinapaz@gmail.com>
2013-10-30 00:14:08 -0700

    feature spec for user creating entries

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,15 +6,35 @@ feature 'user sign in' do
     # create user
   end
 
-  scenario 'my first test' do 
+  scenario 'user signs in' do 
+    user = FactoryGirl.create(:user)  
+
     # user comes to app front page
+    visit root_path
     # user fills in email field with email
+    fill_in 'Email' , with: user.email 
     # user fills in password field with password
+    fill_in 'Password' , with: user.password
     # user clicks log in button
+    click_button 'Sign in'
+
     # user sees listing entries page
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'entries'   
+    
     # user clicks new entry link
     # user creates new entry
     # user clicks save button
+    click_link 'New Entry'
+    
+    fill_in 'Physical location' , with: 'fakeplace'
+    fill_in 'Place' , with: 'fakeplace'
+    fill_in 'Mood' , with: 'fakemood'
+    # todo: test the date select 'Took place at' , with: 'fake_date_time'
+    fill_in 'Positive activity' , with: "fakeactivity"
+    click_button 'Save'
+    
     # user sees listing entries page with new entry included
+    expect(page).to have_content 'fakeactivity'
   end 
 end
